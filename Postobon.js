c320e544d3ee16b3de38866645b8a572556d2f26
Sebastianrubio12 <77504061+Sebastianrubio12@users.noreply.github.com>
2021-03-12 12:49:10 -0500

    ejercicioresuelto
    Ejercicio completado

---
+++ Postobon.js
@@ -0,0 +1,21 @@
+//ENTRADAS
+let horasTrabajadas=40;
+let nombreEmpleado="Juan Gallego Mesa";
+let identificacionTrabajador="898488";
+
+
+//PROCESO
+if(horasTrabajadas<=40){
+    let valorHora=20000;
+    let totalDevengado=valorHora*horasTrabajadas;
+    console.log("Señor: "+nombreEmpleado +" su salario fue de: $"+totalDevengado);
+}else{
+    let valorHoraNormal=20000;
+    let valorHoraExtra=25000;
+    
+    let totalHorasNormales=valorHoraNormal*40;
+    let totalHorasExtra=valorHoraExtra*(horasTrabajadas-40);
+
+    totalDevengado=totalHorasExtra + totalHorasNormales;
+    console.log("Señor: "+nombreEmpleado+" su salario fue de: $"+ totalDevengado);
+}
